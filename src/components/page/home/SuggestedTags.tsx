858ec58a1b69bc721ee97a3b140e9dd27ac51d9d
Randima Dias <randimamethminid525@gmail.com>
2023-12-31 12:38:24 +0530

    feat: create suggestedTags component

--- src/components/page/home/SuggestedTags.tsx
+++ src/components/page/home/SuggestedTags.tsx
@@ -1,5 +1,16 @@
+import DiscoverButton from "@/components/util/DiscoverButton"
+
 export default function SuggestedTags() {
-    return <section className={'flex h-[300px] items-center justify-center bg-pink-700 md:w-[50%] xl:h-[40%] xl:w-auto'}>
-        <h2 className={'text-4xl text-white'}>Suggested Tags</h2>
-    </section>
+    return (
+        <section className={'h-[300px] items-center md:w-[50%] xl:h-[40%] xl:w-auto'}>
+            <h1 className={'pb-4 text-right text-[1.5rem] font-[700]'}>DISCOVERE MORE OF WHAT MATTER TO YOU</h1>
+            <div className="flex justify-end gap-4">
+                <DiscoverButton />
+                <DiscoverButton />
+            </div>
+
+        </section>
+    )
+
+
 }
