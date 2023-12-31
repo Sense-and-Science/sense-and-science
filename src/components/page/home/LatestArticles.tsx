3676d9add9ea6245b9e3dee8814bd94b503e4460
Randima Dias <randimamethminid525@gmail.com>
2023-12-31 12:23:37 +0530

    feat: create author-of-the-month componenet

--- src/components/page/home/LatestArticles.tsx
+++ src/components/page/home/LatestArticles.tsx
@@ -2,7 +2,7 @@ import LatestArticle
  from "@/components/util/LatestArticle"
 export default function LatestArticles() {
     return <section className={'h-[900px] w-full flex-col gap-6 py-4'}>
-        <h1 className={'pb-4 text-[1.5rem] font-[700]'}>Latest Article</h1>
+        <h1 className={'pb-4 text-[1.5rem] font-[700]'}>LATEST ARTICLE</h1>
        <LatestArticle/> 
        <LatestArticle/> 
        <LatestArticle/> 
